d1592c859668b90084c41ea271278c3fea24c1e2
Marijn Adriaanse <m.h.p.adriaanse@student.tudelft.nl>
2024-06-10 22:27:05 +0200

    Pretty much full syntax parsing now

--- rust/proglang/src/main.rs
+++ rust/proglang/src/main.rs
@@ -3,10 +3,10 @@
 mod parser;
 //mod separated_by_save;
 
-use parser::exprparser;
+use parser::parser;
 use chumsky::Parser;
 
 fn main() {
     let src = std::fs::read_to_string("src/test.txt").unwrap();
-    println!("{:?}",exprparser().parse(src));
+    println!("{:#?}",parser().parse(src));
 }
